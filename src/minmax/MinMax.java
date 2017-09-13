af77254434ef31bdc91ee532c070da5dbfcdd88f
Oficial <Oficial@Oficial>
2017-09-13 16:54:29 -0300

    Commit.

--- src/minmax/MinMax.java
+++ src/minmax/MinMax.java
@@ -9,6 +9,7 @@ public class MinMax {
     public static void main(String[] args) {
         System.out.println("MinMax");
         //just a comment
+        //just a comment from netbeans
     }
     
 }
