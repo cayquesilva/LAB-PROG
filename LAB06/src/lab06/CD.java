5b56468cfc2460492e68ab0833a792842cda9552
Oficial <Oficial@Oficial>
2017-11-30 23:59:28 -0300

    Envio do Laboratório 06.

---
+++ LAB06/src/lab06/CD.java
@@ -0,0 +1,163 @@
+/*
+ * Aluno: <David Félix da Silva Filho>
+ */
+package lab06;
+
+import java.util.*;
+
+/**
+ * Classe CD de músicas de um artista.
+ * @author david
+ */
+
+public class CD {
+    
+    private String artista;
+    private String tituloCD;
+    private String musicaPrincipal;
+    private List<String> listaDeMusicas;
+    private int M;
+
+    /**
+     * Construtor que recebe o artista, título e a quantidade de músicas.
+     * @param artista Artista do CD.
+     * @param tituloCD Título do CD do artista/banda/etc;
+     * @param M Quantidade de músicas informada pelo usuário.
+     * @throws Exception Se algum parâmetro passado for inválido, será lançado uma exceção.
+     */
+    public CD(String artista, String tituloCD, int M) throws Exception{   
+        verificaParametros(artista, tituloCD, M);
+        listaDeMusicas  = new ArrayList<>(this.M);
+    }
+
+    /**
+     * Construtor que recebe o artista e o título do CD.
+     * @param artista Artista compositor do CD.
+     * @param tituloCD Título do CD do artista/banda/etc.
+     * @param M A quantidade de músicas é definida como 10.
+     * @throws Exception Se algum parâmetro passado for inválido, será lançado uma exceção.
+     */
+    public CD(String artista, String tituloCD) throws Exception{
+        M = 10;
+        verificaParametros(artista, tituloCD, M); 
+        listaDeMusicas  = new ArrayList<>(this.M);
+    }
+    
+    private void verificaParametros(String artista, String tituloCD, int M) throws Exception{
+        if(tituloCD == null || tituloCD.trim().isEmpty()){
+            throw new Exception ("O nome do CD não pode ser vazio.");
+        }else{
+            this.tituloCD = tituloCD;
+        }
+        if(artista == null || artista.trim().isEmpty()){
+            throw new Exception ("O nome do compositor do CD não pode ser vazio.");
+        }else{
+            this.artista = artista;
+        }
+        if(M <= 0){
+            throw new Exception ("O número de músicas deve ser maior do que 0(zero).");
+        }else{
+            this.M = M;
+        }
+        
+    }
+
+    /**
+     * Retorna o nome do artista.
+     * @return O artista.
+     */
+    public String getArtista() {
+        return artista;
+    }
+
+    /**
+     * Retorna o nome do CD.
+     * @return O nome do CD.
+     */
+    public String getTituloCD() {
+        return tituloCD;
+    }
+
+    /**
+     * Retorna a trilha principal do CD.
+     * @return A trilha principal do CD.
+     */
+    public String getMusicaPrincipal() {
+        return musicaPrincipal;
+    }
+
+    /**
+     * Define alguma música do CD como principal.
+     * @param faixa A música que o usuário deseja colocar como principal.
+     * @return Retorna o resultado da tentativa de alteração.
+     */
+    public boolean setMusicaPrincipal(String faixa){
+        if (listaDeMusicas.contains(faixa)){
+            this.musicaPrincipal = faixa;
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Retorna a i-ésima faixa do CD.
+     * @param indice Se não for válido retorna null.
+     * @return A i-ésima faixa do CD se o índice for válido
+     */ 
+    public String getIesimaMusica(int indice){
+	if(indice >= 0 && indice < listaDeMusicas.size()){
+            return listaDeMusicas.get(indice);
+	}
+	return null;
+    }
+    
+    /**
+     * Cadastra uma nova faixa no CD. 
+     * @param faixa A música a ser cadastrada no CD.
+     * @return true se a música for adicionada com sucesso e false caso não seja adicionada.
+     */
+    public boolean cadastraMusica(String faixa){
+        if(!(faixa == null) && listaDeMusicas.size() < M){
+            listaDeMusicas.add(faixa);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Retorna todas as músicas do CD. 
+     * @return Todas as músicas do CD.
+     */
+    public List<String> getListaDeMusicas() {
+        return listaDeMusicas;
+    }
+    
+    /**
+     * Retorna a quantidade de músicas.
+     * @return A quantidade de músicas.
+     */
+    public int getM() {
+        return M;
+    }
+    
+    /**
+     * Faz a comparação de 2 CD's
+     */
+    @Override
+    public boolean equals(Object cdMusicas){
+        if (cdMusicas instanceof CD && cdMusicas != null){
+            CD cdTemp = (CD) cdMusicas;
+            if (getArtista().equals(cdTemp.getArtista()) && getTituloCD().equals(cdTemp.getTituloCD())){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        return "CD{" + "Artista: " + artista + ", Título do CD: " + tituloCD + ", Música Principal: " + musicaPrincipal + ", Lista de Músicas: " + listaDeMusicas + ", Quantidade de Músicas: " + M + '}';
+    }
+     
+}
---
+++ LAB06/src/lab06/MinhaCDTeca.java
@@ -0,0 +1,142 @@
+/*
+ * Aluno: <David Félix da Silva Filho>
+ */
+package lab06;
+
+import java.util.*;
+
+/**
+ * Classe que representa uma coleção de CD's.
+ * @author david
+ */
+
+public class MinhaCDTeca {
+    private List<CD> listaDeCDs;
+
+    /**
+     * Construtor que inicializa alguma coleção de CD's.
+     */
+    public MinhaCDTeca() {
+        listaDeCDs = new ArrayList<CD>();
+    }
+    
+    /**
+     * Adicionar um CD á lista.
+     * @param cd CD que vai er inserido.
+     */
+    public void adicionaCD(CD cd) throws Exception{
+        if(cd != null){
+            listaDeCDs.add(cd);
+        }else{
+            throw new Exception("Informe um CD válido.");
+        }
+    }
+    
+    /**
+     * Adiciona CD's na coleção
+     * @param listaDeCDs Uma lista de CD's que pertence a coleção.
+     */
+    public void adicionaCDs(List<CD> listaDeCds) throws Exception{
+        if(listaDeCds != null){
+            for(int i = 0; i < listaDeCds.size(); i++){
+                adicionaCD(listaDeCds.get(i));
+            }
+        }
+        
+    }
+    
+    /**
+     * Remove um CD escolhido da lista.
+     * @param tituloCD O CD a ser removido.
+     * @return Retorna o CD que foi removido.
+     */
+    public CD removeCD(String tituloCD){
+        for(int i = 0; i < listaDeCDs.size(); i++){
+            if(tituloCD.equals(listaDeCDs.get(i).getTituloCD())){
+                return listaDeCDs.remove(i);
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Remove múltiplos CD's 
+     * @param listaDeCDs Uma lista de CD's que serão removidos.
+     * @return Se algum CD for removido retorna true senão false.
+     */
+    public boolean removeCDs(List<CD> listaDeCds){
+        boolean remove = false;
+        if(listaDeCds.isEmpty()){
+            for(int i = 0; i < listaDeCds.size(); i++){
+                removeCD(listaDeCds.get(i).getTituloCD());
+                remove = true;
+            }
+        }
+        return remove;
+    }
+    
+    /**
+     * Pesquisa algum CD.
+     * @param nomeCD O CD que deseja pesquisar.
+     * @return O CD se pertencer a coleção e retorna null caso não pertença.
+     */
+    public CD pesquisaCD(String nomeCD){
+        for (int i = 0; i < listaDeCDs.size(); i++){
+            if(nomeCD.equals(listaDeCDs.get(i).getTituloCD())){
+                return listaDeCDs.get(i);
+            }
+        }
+        return null;
+        
+    }
+    
+    /**
+     * A quantidade de CD's na lista da coleção
+     * @return Quantidade de CD's.
+     */
+    public int numeroDeCDs(){
+        return listaDeCDs.size();
+    }
+
+    /**
+     * Retorna de forma organizada as informações dos CD's.
+     * @return as informações dos CD's
+     */
+    @Override
+    public String toString() {
+        return "MinhaCDTeca{" + "Lista de CD's" + listaDeCDs + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.listaDeCDs);
+        return hash;
+    }
+
+    /**
+     * Compara duas CDTecas.
+     * @return Retorna true caso as coleções tenham os mesmos CD's.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MinhaCDTeca other = (MinhaCDTeca) obj;
+        if (!Objects.equals(this.listaDeCDs, other.listaDeCDs)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+
+}
